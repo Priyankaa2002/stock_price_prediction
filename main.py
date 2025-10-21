e9b7e2c1fd86dd38b08076343b6952ff2849beff
Priyanka Prajapati <97026328+Priyankaa2002@users.noreply.github.com>
2025-10-21 16:30:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,78 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas_datareader as data
+import yfinance as yf
+import datetime as dt
+import streamlit as st
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import load_model
+
+
+start = dt.datetime(2014, 1, 1)
+end = dt.datetime(2024, 10, 19)
+
+st.title("Stock Price Prediction")
+user_input = st.text_input("Enter stock ticker", 'AAPL')
+df = yf.download(user_input, start=start, end=end)
+
+st.subheader(" Data from 2014-2024")
+st.write(df.describe())
+
+#Visualization
+st.subheader('Closing Price vs Time chart')
+fig = plt.figure(figsize = (12,8))
+plt.plot(df.Close)
+st.pyplot(fig)
+
+st.subheader('Closing Price vs Time chart with 100MA & 200MA')
+ma100 = df.Close.rolling(100).mean()
+ma200 = df.Close.rolling(200).mean()
+fig = plt.figure(figsize = (12,8))
+plt.plot(ma100, 'r')
+plt.plot(ma200, 'g')
+plt.plot(df.Close, 'b')
+st.pyplot(fig)
+
+
+#Splitting data into x_train and y_train
+data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70): int(len(df))])
+
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+data_training_array = scaler.fit_transform(data_training)
+
+
+#Load my model
+model = load_model('keras_model.h5')
+
+past_100_days = data_training.tail(100)
+final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
+input_data = scaler.fit_transform(final_df)
+
+x_test =[]
+y_test = []
+
+for i in range(100, input_data.shape[0]):
+    x_test.append(input_data[i-100: i])
+    y_test.append(input_data[i, 0])
+
+x_test, y_test = np.array(x_test), np.array(y_test)
+y_predicted = model.predict(x_test)
+
+scaler = scaler.scale_
+scale_factor = 1/scaler[0]
+y_predicted = y_predicted * scale_factor
+y_test = y_test * scale_factor
+
+#predictions
+st.subheader('Predicted Stock Price')
+fig2 = plt.figure(figsize = (12, 6))
+plt.plot(y_test, 'b', label = 'Original Price')
+plt.plot(y_predicted, 'r', label = 'Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+st.pyplot(fig2)
